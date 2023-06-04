13bc5838c5d81c8d54cb512b415b9f8854a7ae6f
青衫客 <2602158283.qq.com>
2023-06-04 09:19:53 +0800

    Layout OK

--- vue-rabbit/src/main.js
+++ vue-rabbit/src/main.js
@@ -5,11 +5,11 @@ import App from './App.vue'
 import router from './router'
 //引入初始化样式文件
 import '@/styles/common.scss'
-//测试接口函数
+/* //测试接口函数
 import{getCategoryAPI} from "@/apis/testAPI"
 getCategoryAPI().then(res=>{
   console.log(res)
-})
+}) */
 
 const app = createApp(App)
 
